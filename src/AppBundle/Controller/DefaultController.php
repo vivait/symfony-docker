5f3088a0a25b18eeb7903831d0f48bed283b9ce4
brunty <matt@mfyu.co.uk>
2016-07-14 20:55:05 +0100

    Adds method to homepage route

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage", methods={"GET"})
      */
     public function indexAction(Request $request)
     {
